b4f05c37335713dcfd303a9554ca380c2efc5f29
Alebs <alebsgd@gmail.com>
2025-02-14 19:14:33 +0100

    route fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,3 @@
 import { Routes } from '@angular/router';
-import { CouponListComponent } from './components/coupon-list/coupon-list.component';
 
-export const routes: Routes = [{ path: '', component: CouponListComponent }];
+export const routes: Routes = [];
