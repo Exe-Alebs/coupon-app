6fb27b9dcc93b44980469c7137aad976112b8961
Alebs <alebsgd@gmail.com>
2025-02-14 18:02:03 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { CouponListComponent } from './components/coupon-list/coupon-list.component';
+
+export const routes: Routes = [{ path: '', component: CouponListComponent }];
